67a2fc10c6cea14e132bd3df43076cdd37c566c8
Michael Beeuwsaert <michael@beeuwsaert.com>
2026-01-09 23:05:26 -0600

    Add mark decoration to make current task state bold.

--- src/editor/logbook_field.ts
+++ src/editor/logbook_field.ts
@@ -1,20 +1,30 @@
-import { StateField, Transaction, Extension, RangeSetBuilder, Line } from '@codemirror/state';
+import { StateField, Transaction, Extension, RangeSetBuilder, RangeSet } from '@codemirror/state';
 import { Decoration, DecorationSet, EditorView } from '@codemirror/view';
 import { TextParseAdapter } from 'logbook/parse_adapter';
 import LogbookParser from 'logbook/logbook_parser';
 import LogbookPluginInterface from 'main';
 import TimeWidget from 'widgets/time_widget';
 import { moment } from 'obsidian';
+import { getWorkflowStatus } from 'tasks/task';
+
+interface LogbookFieldState {
+    decorations: DecorationSet;
+    atomicDecorations: DecorationSet;
+};
 
 export function logbookField(
     plugin: LogbookPluginInterface
 ): Extension {
-    return StateField.define<DecorationSet>({
-        create(state): DecorationSet {
-            return Decoration.none;
+    return StateField.define<LogbookFieldState>({
+        create(state):  LogbookFieldState {
+            return {
+                decorations: Decoration.none,
+                atomicDecorations: Decoration.none,
+            };
         },
-        update(oldState: DecorationSet, transaction: Transaction): DecorationSet {
+        update(oldState: LogbookFieldState, transaction: Transaction): LogbookFieldState {
             const builder = new RangeSetBuilder<Decoration>();
+            const atomicBuilder = new RangeSetBuilder<Decoration>();
 
             const { state } = transaction;
 
@@ -27,23 +37,44 @@ export function logbookField(
 
             for (const book of books) {
                 const from = book.from - 1;
+                
+                const parentLine = doc.lineAt(from);
+                const { text: parentText } = parentLine;
+                const taskState = getWorkflowStatus(parentText, parentLine.from);
 
-                builder.add(
+                atomicBuilder.add(
                     from,
                     from,
                     Decoration.widget({
-                        widget: new TimeWidget(book),
+                        widget: new TimeWidget(book, plugin, taskState),
                     })
                 );
+                
+                if (taskState?.currentStateRange) {
+                    builder.add(
+                        taskState?.currentStateRange?.from,
+                        taskState.currentStateRange.to,
+                        Decoration.mark({
+                            attributes: {
+                                style: "font-weight: bold"
+                            }
+                        })
+                    );
+                }
             }
 
-            return builder.finish();
+            return {
+                decorations: builder.finish(),
+                atomicDecorations: atomicBuilder.finish(),
+            };
         },
-        provide(field: StateField<DecorationSet>): Extension {
+        provide(field: StateField<LogbookFieldState>): Extension {
             return [
-                EditorView.decorations.from(field),
+                EditorView.decorations.from(field, (t: LogbookFieldState) => {
+                    return (RangeSet<Decoration>).join([t.decorations, t.atomicDecorations]);
+                }),
                 EditorView.atomicRanges.of(
-                    v => v.state.field(field)
+                    v => v.state.field(field).atomicDecorations
                 )
             ];
         }
